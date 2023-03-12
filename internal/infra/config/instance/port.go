745fa4ffc1421d8017a080c9379215463b9bfcb2
Javier Salazar <jsalazar@cleveritgroup.com>
2023-03-12 11:48:38 -0300

    Initial Implementation DB conextion with github.com/jinzhu/gorm

--- internal/infra/config/instance/port.go
+++ internal/infra/config/instance/port.go
@@ -1,11 +1,37 @@
 package instance
 
-import "cleverbank/internal/core/port"
+import (
+	"cleverbank/internal/core/port"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"log"
+)
+
+func GetDatabaseConextion() *gorm.DB {
+	databasename := "cleverbank"
+	database := "postgres"
+	databasepassword := "postgres"
+	databaseurl := "postgres://postgres:" + databasepassword + "@localhost/" + databasename + "?sslmode=disable"
+
+	connection, err := gorm.Open(database, databaseurl)
+	if err != nil {
+		log.Fatalln("Invalid database url")
+	}
+	sqldb := connection.DB()
+
+	err = sqldb.Ping()
+	if err != nil {
+		log.Fatal("Database connected")
+	}
+	fmt.Println("Database connection successful.")
+	return connection
+}
 
 func GetAccountMovementPort() port.AccountMovementPort {
 	return AccountMovementRepository{}
 }
 
 func GetAccountInfoPort() port.AccountInfoPort {
-	return AccountInfotRepository{}
+	return AccountInfotRepository{GetDatabaseConextion()}
 }
--- internal/infra/config/instance/usecase.go
+++ internal/infra/config/instance/usecase.go
@@ -3,6 +3,7 @@ package instance
 import (
 	"cleverbank/internal/core/implementation"
 	"cleverbank/internal/core/usecase"
+	"github.com/jinzhu/gorm"
 )
 
 func GetAccountMovementUseCase() usecase.AccountMovementUseCase {
@@ -21,9 +22,21 @@ func (amr AccountMovementRepository) Deposit(accountId string) (string, error) {
 	return "movementID-Generated", nil
 }
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 type AccountInfotRepository struct {
+	dbconex *gorm.DB
 }
 
-func (amr AccountInfotRepository) GetBalance(accountNumber string) (string, error) {
-	return "$ 1.550.6624", nil
+func (air AccountInfotRepository) GetBalance(accountNumber string) (string, error) {
+	var dbuser User
+	air.dbconex.Where("email = ?", "user@test.com").First(&dbuser)
+	return dbuser.Password, nil
+}
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Email    string `gorm:"unique" json:"email"`
+	Password string `json:"password"`
+	Role     string `json:"role"`
 }
